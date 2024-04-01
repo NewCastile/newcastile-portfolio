d1e43005462d211d31d46c727ce76656f5bf1c88
NewCastile <carlosaguanipa1@gmail.com>
2024-04-01 13:23:07 -0400

    first commit

---
+++ src/types/index.ts
@@ -0,0 +1,45 @@
+import type { ImageInputFormat } from "astro";
+
+export interface ImageMetadata {
+    src: string;
+    width: number;
+    height: number;
+    format: ImageInputFormat;
+    orientation?: number;
+}
+
+export interface TechnologyGroupItem {
+    name: string;
+    icon: {
+        src: ImageMetadata;
+        size: number;
+        background?: boolean;
+    };
+}
+
+export interface TechnologyGroup {
+    name: string;
+    items: TechnologyGroupItem[];
+}
+
+export interface Project {
+    name: string;
+    description: string;
+    image: ImageMetadata;
+    technologies: Array<TechnologyGroupItem>;
+    repo: string;
+    web?: string;
+}
+
+export interface Course {
+    name: string;
+    teachers: [string];
+    certificateSrc: ImageMetadata;
+    link: string;
+}
+
+export interface NavItems {
+    title: string;
+    label: string;
+    url: string;
+}
