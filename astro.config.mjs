ae6f6f508cfcb73b94b6dc42b85ec65b6628aff3
NewCastile <carlosaguanipa1@gmail.com>
2024-04-01 01:06:31 -0400

    update for internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
   integrations: [tailwind(), react({ include: ['**/react/*'], experimentalReactChildren: true })],
   image: {
     domains: ["astro.build"]
